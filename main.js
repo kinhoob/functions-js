0049323f31cf49353a053660c3d6585320a97af6
Marco Davi <102377212+kinhoob@users.noreply.github.com>
2023-12-08 13:05:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
